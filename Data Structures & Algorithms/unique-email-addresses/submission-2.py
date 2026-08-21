b26fc3a05ab9709d99e68e34583fdd926426a85a
Rafal Kaczynski <69152315+rafkac@users.noreply.github.com>
2026-08-21 19:23:54 +0100

    Add: unique-email-addresses - submission-2

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        uniques = []
+
+        for e in emails:
+            username, domain = (e.split("@"))
+            username = username.replace(".", "")
+            if "+" in username:
+                username = (username.split("+"))[0]
+
+            if username + domain not in uniques:
+                uniques.append(username + domain)
+
+        print(uniques)
+
+
+        return len(uniques)
